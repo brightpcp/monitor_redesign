add27173351d75d806755dbbccdd83fb690efae6
brightpcp <pichapat42@gmail.com>
2022-08-03 13:12:32 +0700

    redesign

---
+++ m.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Myfriend</title>
+</head>
+<body>
+<div class="bg">
+    <img src="./images/Vector-top.svg" alt="" class="bg-top">
+    <img src="./images/Vector-bot.svg" alt="" class="bg-bot">    
+</div>
+<div class='containor'>
+  <div class='row'>
+    <div class='column'>
+      <div class='icon-column detail-column'>
+        <img src="./images/logo.svg" alt="logo" class="center">
+        <div class="text-detail">
+            <h1>1.24</h1>
+            <h3>Current Release</h3>
+        </div>
+      </div>
+    </div>
+    <div class='column'>
+      <div class='registeredUsers-column detail-column'>
+      <img src="./images/users.svg" alt="users" class="center">
+        <div class="text-detail">
+            <h1>9576</h1>
+            <h3>Registered Users</h3>
+        </div>
+      </div>
+    </div>
+    <div class='column'>
+      <div class='registeredPets-column detail-column'>
+      <img src="./images/pets.svg" alt="pets" class="center">
+        <div class="text-detail">
+            <h1>9576</h1>
+            <h3>Registered Pets</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='row'>
+    <div class='column'>
+      <div class='Days-column detail-column'>
+      <img src="./images/days.svg" alt="days" class="center">
+        <div class="text-detail">
+            <h1>9576</h1>
+            <h3>Day since Release</h3>
+        </div>
+      </div>
+    </div>
+    <div class='column'>
+      <div class='ios-column detail-column'>
+      <img src="./images/ios.svg" alt="ios" class="center">
+        <div class="text-detail">
+            <h1>1.24</h1>
+            <h3>iOS Installations</h3>
+        </div>
+      </div>
+    </div>
+    <div class='column'>
+      <div class='android-column detail-column'>
+      <img src="./images/android.svg" alt="android" class="center">
+        <div class="text-detail">
+            <h1>1.24</h1>
+            <h3>Android Installations</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
